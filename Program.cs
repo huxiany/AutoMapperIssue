1e7b34a1283c4e4ba7d3b93fb609f2e444a4f570
胡先艳 <huxiany@outlook.com>
2018-06-06 09:51:43 +0800

    about AutoMapper Issue

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using AutoMapper;
+
+namespace AutoMapperIssue
+{
+    class Student
+    {
+        public string Class { get; set; }
+        public JsonObject<UserInfo> UserInfo { get; set; }
+    }
+
+    public class UserInfo
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    class StudentDTO
+    {
+        public string Class { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    class AutoMapperProfileConfiguration : Profile
+    {
+        public AutoMapperProfileConfiguration()
+        : this("MyProfile")
+        {
+        }
+        protected AutoMapperProfileConfiguration(string profileName)
+        : base(profileName)
+        {
+            CreateMap<Student, StudentDTO>()
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.UserInfo.Object.Name))
+            .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.UserInfo.Object.Age));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mapper.Initialize(x => x.AddProfile<AutoMapperProfileConfiguration>());
+
+            var student = new Student
+            {
+                Class = "Grade 5",
+                UserInfo = new UserInfo
+                {
+                    Name = "Rachel",
+                    Age = 10
+                }
+
+            };
+
+            var studentDTO = Mapper.Map<StudentDTO>(student);
+            Console.WriteLine("Class = {0}", studentDTO.Class);
+            Console.WriteLine("Name = {0}", studentDTO.Name);
+            Console.WriteLine("Age = {0}", studentDTO.Age);
+        }
+    }
+}
